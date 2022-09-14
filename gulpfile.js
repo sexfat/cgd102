0f5c860068cc0b8b3a80297f0d77827c67bb2d76
sexfat <bryant6831@gmail.com>
2022-09-14 15:13:07 +0800

    add

--- gulpfile.js
+++ gulpfile.js
@@ -12,37 +12,16 @@ function task(cb) {
     console.log('gulp start');
     cb();
 }
-//私有任務
-
-//任務輸出
-exports.do = task
-
-//任務 檔案搬家
-function move() {
-    return src('src/index.html').pipe(dest('dest'))
-}
 
 
-exports.mv = move;
 
 
-// 
-function taskA(cb) {
-    console.log('a mission');
-    cb();
-}
 
 
-function taskB(cb) {
-    console.log('b mission');
-    cb();
-}
 
-//同步
-exports.sync = parallel(taskA, taskB);
 
-// 非同步
-exports.async = series(taskA, taskB);
+
+
 
 // 壓縮 美化css
 const cleanCSS = require('gulp-clean-css');
@@ -73,20 +52,6 @@ function ugJS() {
 
 exports.miniJs = ugJS;
 
-
-// 改名
-// const rename = require('gulp-rename');
-// function change(){
-//     return src('dest/js/*.js')
-//            .pipe(rename({
-//             extname: '.min.js'
-//            }))
-//            .pipe(dest('dest/js/'))
-
-// }
-
-//exports.js = series(ugJS ,change) //先執行壓縮js 再更名min.js
-exports.js = ugJS
 
 
 
@@ -127,27 +92,6 @@ function img() {
 
 
 
-function w() {
-    watch(['./src/sass/*.scss', './src/sass/**/*.scss'], styleSass);
-    watch(['./src/*.html', './src/layout/*.html'], htmltemplate);
-    watch(['./src/js/*.js', './src/js/**/*.js'], ugJS);
-    watch(['./src/images/*.*', './src/images/**/*.*'], img);
-}
-// 先打包在監看變動
-exports.dev = series(parallel(htmltemplate, styleSass, ugJS, img), w)
-
-// 多隻css整合
-var concat = require('gulp-concat');
-
-function concatcss() {
-    return src(['dest/css/*.css', '!dest/css/all.css'])
-        .pipe(concat('all.css'))
-        .pipe(dest('dest/css/'))
-}
-
-exports.allcss = concatcss;
-
-
 
 // 瀏覽器同步
 const browserSync = require('browser-sync');
@@ -168,8 +112,6 @@ function browser(done) {
     watch(['./src/images/*.*', './src/images/**/*.*'], img).on('change', reload)
     done();
 }
-
-exports.default = series(parallel(htmltemplate, styleSass, ugJS, img), browser);
 
 
 //上線使用  css跨瀏覽器
@@ -223,6 +165,8 @@ function clear() {
 }
 exports.cls  = clear;
 
+// 開發用
+exports.default = series(parallel(htmltemplate, styleSass, ugJS, img), browser);
 
 // 上線用
 exports.online = series(clear, parallel(htmltemplate, styleSass,babel5 , img_c))
